1f1c499ea879ca10dc00719919c4277d1f32f1fb
Felipe <cadavidf@users.noreply.github.com>
2020-09-28 02:43:48 -0500

    Create OrganizeFiles.py
    initial commit. Automate Borring Stuff with Python

---
+++ OrganizeFiles.py
@@ -0,0 +1,36 @@
+import os
+import shutil import move
+from pathlib import Path
+SUBDIRECTORIES = {
+    "DOCUMENTS": ['.pdf','.rtf','.txt'],
+    "AUDIO":['.m4a','.m4b','.mp3'],
+    "VIDEOS": ['.mov','.avi','.mp4'],
+    "IMAGES": ['.jpg','.jpeg','.png']
+}
+def pickDirectory(value):
+    for category, suffixes in SUBDIRECTORIES.items():
+        for suffix in suffixes:
+            if suffix == value:
+                return category
+    return 'MISC' #If filetype doesn't exist in our dictionary
+
+def organizeDirectory():
+    for item in os.scandir():
+        if item.is_dir():
+           
+        filePath = Path(item)
+        filetype = filePath.suffix.lower()
+        directory = pickDirectory(filetype)
+        directoryPath = Path(directory)
+        if directoryPath.is_dir() != True:
+            directoryPath.mkdir()
+        filePath.rename(directoryPath.joinpath(filePath))
+
+def moveFilesOutOfDirectory():
+    for item in os.scandir():
+        if item.is_dir():
+            filePath = Path(item)
+        
+
+
+organizeDirectory()
